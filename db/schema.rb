a54032c0e393826bb1661c8a9b9c61a24187d740
rexcklee <rexcklee852@gmail.com>
2024-10-21 10:02:02 -0500

    generated Street AR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_21_144857) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_21_150127) do
+  create_table "streets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "time_limits", force: :cascade do |t|
     t.string "time_period"
     t.datetime "created_at", null: false
