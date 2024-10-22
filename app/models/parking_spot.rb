fe95711458f2d28fb83ceee5496af4d456ee154c
rexcklee <rexcklee852@gmail.com>
2024-10-21 22:23:40 -0500

    added association for parking_spot to pay_type

--- app/models/parking_spot.rb
+++ app/models/parking_spot.rb
@@ -4,5 +4,8 @@ class ParkingSpot < ApplicationRecord
   belongs_to :hourly_rate
   belongs_to :business_zone
 
+  has_many :parking_spot_pay_type
+  has_many :pay_type, through :parking_spot_pay_type
+
   validates :block_number, :payment_time, :total_space, :mobile_payment_number, presence: true
 end
--- app/models/pay_type.rb
+++ app/models/pay_type.rb
@@ -1,2 +1,4 @@
 class PayType < ApplicationRecord
+    has_many :parking_spot_pay_type
+    has_many :parking_spot, through :parking_spot_pay_type
 end
