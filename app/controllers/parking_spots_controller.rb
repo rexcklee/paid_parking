875663c8c4a1c4c4866bf897ce745057e0e5bcaf
rexcklee <rexcklee852@gmail.com>
2024-10-22 22:32:24 -0500

    created Parking Spots index table

--- app/controllers/parking_spots_controller.rb
+++ app/controllers/parking_spots_controller.rb
@@ -1,8 +1,9 @@
 class ParkingSpotsController < ApplicationController
   def index
-    @parking_spots = ParkingSpot.all
+    @parking_spots = ParkingSpot.includes(:time_limit, :street, :business_zone, :hourly_rate).all
   end
 
   def show
+    @parking_spot = ParkingSpot.find(params[:id])
   end
 end
--- app/models/parking_spot.rb
+++ app/models/parking_spot.rb
@@ -4,8 +4,12 @@ class ParkingSpot < ApplicationRecord
   belongs_to :hourly_rate
   belongs_to :business_zone
 
-  has_many :parking_spot_pay_type
-  has_many :pay_type, through: :parking_spot_pay_type
+  has_many :parking_spot_pay_types
+  has_many :pay_types, through: :parking_spot_pay_types
 
   validates :block_number, :payment_time, :total_space, :mobile_payment_number, presence: true
+
+  def pay_types_list
+    pay_types.map(&:name).join(", ")
+  end
 end
